65eeff9cd661ef0b6472c10135cca727cf99b524
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-09-29 02:45:34 -0500

    Prototype 3 Part 1 - Complete

---
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30f;
+    private float leftBound = -15;
+
+    private PlayerController playerControllerScript;
+
+    private void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        //if we are out of bounds to the left and the gameObject is an obstacle, destroy this gameObject
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}

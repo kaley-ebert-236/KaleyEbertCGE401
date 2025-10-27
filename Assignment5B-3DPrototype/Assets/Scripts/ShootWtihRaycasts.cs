d754e2e77ddd6c9626074ff42a8ed624a0b93b24
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-10-27 01:46:51 -0500

    Add gun with particle affects, 1:26:35 in follow along

---
+++ Assignment5B-3DPrototype/Assets/Scripts/ShootWtihRaycasts.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWtihRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+    public ParticleSystem muzzleFlash;
+    public float hitForce = 10f; //f indicates that it's a float
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //At the beginning of the Shoot() method, play particle effect
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+            //Get the Target script off the hit object
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            //If a Target script was found, make the target take damage 
+            if (target !=null)
+            {
+                target.TakeDamage(damage);
+
+                //If the shot hits a Rigidbody, apply a force
+                if(hitInfo.rigidbody != null)
+                {
+                    hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+                }
+
+
+            }
+        }
+    }
+}

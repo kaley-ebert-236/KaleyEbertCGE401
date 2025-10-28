64947623166f7f65354b66a2441ccc2fcc45421c
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-10-28 16:14:01 -0500

    Level finished, built out to itch.io

---
+++ Assignment5B-3DPrototype/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinCondition : MonoBehaviour
+{
+    public GameObject winPanel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (winPanel != null)
+            {
+                winPanel.SetActive(true);
+            }
+        }
+    }
+    /*
+    public GameObject winTextObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}

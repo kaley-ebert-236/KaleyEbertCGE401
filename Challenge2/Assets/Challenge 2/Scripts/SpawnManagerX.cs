4cc35ec5660bd6a566dabb9ce33d117b36f44a03
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-11-09 17:14:08 -0600

    Almost done with Challenge 2

--- Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,18 +10,37 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    //private float startDelay = 1.0f;
+    //private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        //StartCoroutine(SpawnRandomBallWithCoroutine());
     }
+
+    /*
+    IEnumerator SpawnRandomBallWithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (!healthSystem.gameOver)
+        {
+            SpawnRandomBall();
+
+            float randomDelay = Random.Range(3f, 5f);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+    */
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall()
     {
+        int prefabIndex = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 

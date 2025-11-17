9f6eafe7e151a05e94bdb96eaeb3d99e7c72048f
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-11-17 00:37:28 -0600

    this is actually driving me crazy

---
+++ PennyPixel_2DTilemapProject/PennyPixel_2DTilemapProject/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinCondition : MonoBehaviour
+{
+    public GameObject winTextGameObject;
+
+    void Start()
+    {
+        if (winTextGameObject != null)
+        {
+            winTextGameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (winTextGameObject != null)
+            {
+                winTextGameObject.SetActive(true);
+            }
+            Debug.Log("Player entered the win zone!");
+        }
+    }
+}

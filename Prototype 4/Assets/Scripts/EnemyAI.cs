7bdc7e5a4ad4a3d3015247b275a10bc84ce96f01
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-11-17 18:29:12 -0600

    Just over halfway through follow along

---
+++ Prototype 4/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Rigidbody enemyRB;
+    public GameObject player;
+    public float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Add force toward the direction from the player to the enemy 
+
+        //vector for direction from enemy to palyer
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        //add force towards player
+        enemyRB.AddForce(lookDirection * speed);
+    }
+}
---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float speed;
+    private float forwardInput;
+
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+
+    private void FixedUpdate()
+    {
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
+}

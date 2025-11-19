81852c67c83f6ae4637063983af4ea090298eef0
kaley-ebert <158327441+kaley-ebert@users.noreply.github.com>
2025-11-19 02:18:18 -0600

    Girl I'm tryin

---
+++ PennyPixel_2DTilemapProject/PennyPixel_2DTilemapProject/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class TriggerZone : MonoBehaviour
+{
+    //public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            ScoreManager.score++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
